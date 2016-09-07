4277d3f5f6f6c608fbc6def75934a82f9bf06eb0
Daniel <daniel.rowland@gmail.com>
2016-09-06 23:10:23 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@
 #Zen Desk API - https://developer.zendesk.com/rest_api/docs
 #Python 3
 
+import pycurl
+curl https://airwatch.zendesk.com/api/v2/tickets/{ticket_id}/metrics.json -v -u {email_address}:{password}
+
